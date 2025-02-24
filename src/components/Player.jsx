90496d694f317fd5ec634102fa112129026c0a80
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-02-24 16:19:34 +0300

    Refactor Player component to use ref for player name input

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,22 +1,21 @@
-import {useState} from "react";
+import {useState, useRef} from "react";
 
 export default function Player() {
-    const [enteredPlayerName, setEnteredPlayerName] = useState('');
-    const [submitted, setSubmitted] = useState(false);
+    const playerName = useRef();
+    const [enteredPlayerName, setEnteredPlayerName] = useState(null);
 
-    function handleChange(event){
-        setEnteredPlayerName(event.target.value);
-        setSubmitted(false);
-    }
     function handleClick(){
-        setSubmitted(true);
+        setEnteredPlayerName(playerName.current.value);
     }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName : 'unknown entity'}</h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" onChange={handleChange} value={enteredPlayerName} />
+        <input
+            type="text"
+            ref={playerName}
+        />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
